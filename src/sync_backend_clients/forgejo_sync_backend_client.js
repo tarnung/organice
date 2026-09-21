05752fc51e34cc3bf635a438ec98db3328c99455
Martin Jung <martin.jung@tarnung.net>
2026-09-21 19:59:06 +0200

    use actually unique ids in file browser

--- src/sync_backend_clients/forgejo_sync_backend_client.js
+++ src/sync_backend_clients/forgejo_sync_backend_client.js
@@ -17,7 +17,7 @@ export const contentsResponseToDirectoryListing = (contents) => {
     contents
       .filter((it) => isDirectory(it) || it.name.match(orgFileExtensions))
       .map((it) => ({
-        id: it.sha,
+        id: `${it.path}-${it.sha}`,
         name: it.name,
         // Organice requires a leading "/", whereas Forgejo API doesn't
         // use one.
